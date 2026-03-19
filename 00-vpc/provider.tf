3bdecc70e23a129b8c69c5d989c47c6f5ab64e10
Nageswar <nagesh23479@gmail.com>
2026-03-19 11:42:26 +0530

    Test

--- 00-vpc/provider.tf
+++ 00-vpc/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "6.33.0" # Terraform AWS provider version
     }
   }
-}
 
+  backend "s3" {
+    bucket  = "sandh-roboshop-dev-infra" # Replace with your unique bucket name
+    key     = "roboshop-dev-vpc"
+    region  = "us-east-1"
+    encrypt = true
+    use_lockfile   = true
+  }
+}
 provider "aws" {
   region = "us-east-1"
 }
