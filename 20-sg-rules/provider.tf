aa743a278b43fc52c4ea2a0240d2b69eff54810d
Nageswar <nagesh23479@gmail.com>
2026-03-21 08:04:22 +0530

    Test

--- 20-sg-rules/provider.tf
+++ 20-sg-rules/provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket  = "sandh-roboshop-dev" # Replace with your unique bucket name
-    key     = "roboshop-dev-vpc"
+    key     = "roboshop-dev-sg-rules"
     region  = "us-east-1"
     encrypt = true
     use_lockfile   = true
