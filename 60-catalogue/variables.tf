a2b715b79539e515aad0a1fd2a0f0dfc5a54cfae
Nageswar <nagesh23479@gmail.com>
2026-03-22 08:09:25 +0530

    Test

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,18 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z086001213PRS8PI96NCE"
+}
+
+variable "domain_name" {
+    default = "sandh.co.in"
+}
+variable "app_version" {
+    default = "v3"
+}
