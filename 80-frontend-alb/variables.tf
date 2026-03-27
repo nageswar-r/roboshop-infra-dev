ea45c5dc20a3afca3f0ac38c8527e9b1c4ccba0b
Nageswar <nagesh23479@gmail.com>
2026-03-27 10:17:18 +0530

    Test

---
+++ 80-frontend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z086001213PRS8PI96NCE"
+}
+
+variable "domain_name" {
+    default = "sandh.co.in"
+}
